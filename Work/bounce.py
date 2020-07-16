18fa4052d5af40d17a0ca88b2cd23842abbd21e2
Soheil-mm <smousavimogh@mun.ca>
2020-07-16 05:13:42 -0230

    exersice 1.5

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,10 @@
 # bounce.py
 #
 # Exercise 1.5
+height = 100.0
+bounce_back = 0
+
+while bounce_back < 10:
+    bounce_back = bounce_back + 1
+    height = height * 0.6
+    print(bounce_back, round(height , 4))
