d3634a6f89b6f9adde82a159de3cd983f7ec391f
Soheil-mm <smousavimogh@mun.ca>
2020-07-16 08:55:01 -0230

    exersice 1.8

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,19 @@
 # mortgage.py
 #
-# Exercise 1.7
+# Exercise 1.8
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+months = 0
+
+while principal > 0:
+
+    if months < 12:
+        principal = principal - 1000
+        total_paid = total_paid + 1000  
+    months = months + 1
+    principal = principal * (1+rate/12) - payment
+    total_paid = total_paid + payment
+
+print('Total paid', round(total_paid, 2) , 'over', months, 'months')
