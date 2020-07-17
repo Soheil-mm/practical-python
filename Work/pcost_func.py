a420fbf3675bd9fe331d978fe2deb763026fa167
Soheil-mm <smousavimogh@mun.ca>
2020-07-17 18:33:38 -0230

    exercise 1.33

--- Work/pcost_func.py
+++ Work/pcost_func.py
@@ -1,7 +1,7 @@
 # pcost_func.py
 #
 # Exercise 1.30
-def portfolio_cost(share):
+def portfolio_cost(filename):
         cost_all = 0.0
 
     with open('Data/portfolio.csv', 'rt') as f:
@@ -11,6 +11,10 @@ def portfolio_cost(share):
             share_count = int(row[1])
             share_price = float(row[2])
             cost_all = cost_all + (share_count * share_price)
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
 
-cost = portfolio_cost('Data/portfolio.csv')
+cost = portfolio_cost(filename)
 print('Total cost:', cost)
